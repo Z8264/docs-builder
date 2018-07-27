cf71fd9a26f4f90f3b120a60e169bf6854ff6e90
张博 <zhangbo@panda.tv>
2018-07-27 22:43:57 +0800

    Signed-off-by: 张博 <zhangbo@panda.tv>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+// css: markdown
 import "github-markdown-css";
+// css: highlight
 import "highlightjs/styles/default.css";
+// css: fit for Chinese
 import "./style.less";
 
 import marked from "marked";
@@ -7,8 +10,12 @@ import hljs from "highlightjs";
 
 function html(dom, md = "") {
   if (!dom) return;
-  dom.innerHTML = marked(md);
-  hljs.initHighlightingOnLoad();
+  let options = {
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  }
+  dom.innerHTML = `<article class="markdown-body">${marked(md, options)}</article>`;
 }
 
 export { html };
